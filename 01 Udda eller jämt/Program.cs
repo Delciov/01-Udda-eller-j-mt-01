7c5d9f6985e8c9fbf850bf9d5ae44d60cdac0eaa
DelcioDevSecOps <delciodevsecops@outlook.com>
2025-09-17 09:15:27 +0200

    Lagt till kod för övning 1

--- 01 Udda eller jämt/Program.cs
+++ 01 Udda eller jämt/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ *  1. Skriv ett uttryck som kollar om ett tal är udda eller jämnt.
+ */
+
+Console.Write("Ange ett tal: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Talet {0} är 1", number, number % 2 == 0 ? "jämt" : "udda");
+ 
---
+++ 02 Dividera med 7 och 5/Program.cs
@@ -0,0 +1,3 @@
+﻿/*
+ * 1.   Skriv ett uttryck som kollar om ett givet  är delbart med 7 och 5.
+ */
